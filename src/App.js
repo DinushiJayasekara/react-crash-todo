94e67a1c9c168ccc5d9a9e56eeb3d563b335dc84
DinushiJayasekara <jkdrj02@gmail.com>
2020-10-21 20:09:43 +0530

    change-state-todo-item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-
 import Todos from './components/Todos'
+import React, { Component } from 'react';
 
 import './App.css';
 
@@ -25,10 +24,21 @@ class App extends Component {
     ]
   }
 
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos}></Todos>
+        <Todos todos={this.state.todos} markComplete={this.markComplete}></Todos>
       </div>
     );
   }
