fa7ed38b8de339a41c23473cba18a5d7908db325
DinushiJayasekara <jkdrj02@gmail.com>
2020-10-21 13:33:52 +0530

    use-props

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,13 +1,19 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react'
 
-export class Todoitem extends Component {
+export class TodoItem extends Component {
     render() {
         return (
-            <div>
+            <div style={{ backgroundColor: '#f4f4f4' }}>
                 <p>{ this.props.todo.title }</p>
             </div>
         )
     }
 }
 
-export default Todoitem
+// PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+export default TodoItem
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,19 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 import TodoItem from './TodoItem';
 
 class Todos extends Component {
     render() {
-        return this.props.todos.map((todo)=>(
+        return this.props.todos.map((todo) => (
             <TodoItem key={todo.id} todo={todo}></TodoItem>
         ));
     }
+}
+
+// PropTypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
 }
 
 export default Todos;
